65e1d462272e94d8a074ffed8138e1cdd23324f6
Alexander Frazer <afrazer@protonmail.com>
2025-01-02 09:28:09 -0500

    Fix the syntax highlighting for  in C++ ...

--- nvim/lua/plugins/dracula.lua
+++ nvim/lua/plugins/dracula.lua
@@ -5,6 +5,13 @@ return {
   name = "dracula",
   priority = 1000,
   config = function()
+    require("dracula").setup({
+      overrides = function(colors)
+        return {
+          ['@type.builtin'] = { fg = colors.cyan, italic = false }
+        }
+      end
+    })
     vim.cmd.colorscheme "dracula"
   end
 }
