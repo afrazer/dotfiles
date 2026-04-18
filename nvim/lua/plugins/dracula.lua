46334d675ee1520e22f5db38544558014e934158
Alexander Frazer <afrazer@protonmail.com>
2026-04-18 12:10:36 -0400

    Formatting

--- nvim/lua/plugins/dracula.lua
+++ nvim/lua/plugins/dracula.lua
@@ -1,17 +1,16 @@
-
 return {
-  "Mofiqul/dracula.nvim",
-  lazy = false,
-  name = "dracula",
-  priority = 1000,
-  config = function()
-    require("dracula").setup({
-      overrides = function(colors)
-        return {
-          ['@type.builtin'] = { fg = colors.cyan, italic = false }
-        }
-      end
-    })
-    vim.cmd.colorscheme "dracula"
-  end
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+    name = "dracula",
+    priority = 1000,
+    config = function()
+        require("dracula").setup({
+            overrides = function(colors)
+                return {
+                    ["@type.builtin"] = { fg = colors.cyan, italic = false },
+                }
+            end,
+        })
+        vim.cmd.colorscheme("dracula")
+    end,
 }
--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,54 +1,54 @@
 return {
-	"nvim-mini/mini.nvim",
-	version = "*",
-	config = function()
-		require("mini.basics").setup()
-		require("mini.extra").setup()
-		require("mini.files").setup()
-		require("mini.icons").setup()
-		require("mini.git").setup()
-		require("mini.diff").setup()
-		require("mini.diff").setup({
-			view = {
-				signs = {
-					add = " ",
-					change = " ",
-					delete = " ",
-				},
-			},
-		})
-		require("mini.indentscope").setup()
-		require("mini.statusline").setup()
-		require("mini.cmdline").setup()
-		require("mini.ai").setup()
-		require("mini.jump2d").setup({
-			view = {
-				dim = true,
-				n_steps_ahead = 1,
-			},
-		})
+    "nvim-mini/mini.nvim",
+    version = "*",
+    config = function()
+        require("mini.basics").setup()
+        require("mini.extra").setup()
+        require("mini.files").setup()
+        require("mini.icons").setup()
+        require("mini.git").setup()
+        require("mini.diff").setup()
+        require("mini.diff").setup({
+            view = {
+                signs = {
+                    add = " ",
+                    change = " ",
+                    delete = " ",
+                },
+            },
+        })
+        require("mini.indentscope").setup()
+        require("mini.statusline").setup()
+        require("mini.cmdline").setup()
+        require("mini.ai").setup()
+        require("mini.jump2d").setup({
+            view = {
+                dim = true,
+                n_steps_ahead = 1,
+            },
+        })
 
-		vim.keymap.set("n", "<C-n>", function()
-			local mf = require("mini.files")
-			if not mf.close() then
-				mf.open(vim.api.nvim_buf_get_name(0), true)
-			end
-		end)
+        vim.keymap.set("n", "<C-n>", function()
+            local mf = require("mini.files")
+            if not mf.close() then
+                mf.open(vim.api.nvim_buf_get_name(0), true)
+            end
+        end)
 
-		vim.keymap.set("n", "g]", function()
-			require("mini.diff").goto_hunk("next")
-		end, {
-			desc = "Next hunk",
-		})
+        vim.keymap.set("n", "g]", function()
+            require("mini.diff").goto_hunk("next")
+        end, {
+            desc = "Next hunk",
+        })
 
-		vim.keymap.set("n", "g[", function()
-			require("mini.diff").goto_hunk("prev")
-		end, {
-			desc = "Prev hunk",
-		})
+        vim.keymap.set("n", "g[", function()
+            require("mini.diff").goto_hunk("prev")
+        end, {
+            desc = "Prev hunk",
+        })
 
-		vim.keymap.set("n", "<leader>s", "cmd>lua MiniJump2d.start()<cr>", {
-			desc = "Start Jumping",
-		})
-	end,
+        vim.keymap.set("n", "<leader>s", "cmd>lua MiniJump2d.start()<cr>", {
+            desc = "Start Jumping",
+        })
+    end,
 }
