4d0460b07e3d176003895ec80df73aec1976854b
Alexander Frazer <afrazer@protonmail.com>
2026-04-19 14:39:37 -0400

    Make CursorLine less annoyingly bright

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,13 @@
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,3 +15,4 @@ require("vim-options")
 
 require("lazy").setup("plugins")
 
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2a2a2a" })
